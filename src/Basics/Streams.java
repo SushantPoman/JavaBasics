be1f83ddb4a8240935ced6165b8f58620bd8b7ba
SushantPoman <sushantpoman94@gmail.com>
2024-10-17 09:44:49 +0530

    Operation on string using streams

--- src/Basics/Streams.java
+++ src/Basics/Streams.java
@@ -1,13 +1,12 @@
 package Basics;
 
 import java.util.*;
+import java.util.function.Function;
 import java.util.stream.Collectors;
-import java.util.stream.IntStream;
-import java.util.stream.Stream;
 
 public class Streams {
 
-    public static void main(String args[]){
+    public static void main(String args[]) {
 
 //        Sum using array
         /*int[] numArray = {3, 6, 2, 5, 2};
@@ -36,7 +35,22 @@ public class Streams {
                 .stream()
                 .map(Object::toString)
                 .collect(Collectors.joining(", "));
-        System.out.println("Cities: "+cityNames);
+        System.out.println("Cities: " + cityNames);
+
+//        Find out first non repeated char
+        String name = "Desserted";
+        Map<Character, Long> map = name.chars().mapToObj(c -> (char) c).collect(Collectors.groupingBy(Function.identity(), Collectors.counting()));
+        Character firstNon = map.entrySet().stream().filter(e -> e.getValue() == 1).findFirst().get().getKey();
+        System.out.println("firstNon - " + firstNon);
+
+//        Sort by descending count of character occurrences
+        String countLetters = "Sushantsunilpoman";
+        Map<Character, Long> countMap = countLetters.chars().mapToObj(e -> (char) e).collect(Collectors.groupingBy(e -> e, LinkedHashMap::new, Collectors.counting()));
+        Map<Character, Long> maxCount = countMap.entrySet().stream().sorted(Map.Entry.<Character, Long>comparingByValue().reversed())
+                .collect(Collectors.toMap(e -> e.getKey(), e -> e.getValue(), (a, b) -> b, LinkedHashMap::new));
+        maxCount.forEach((k, v) -> System.out.println(k + " " + v));
+
+
 
     }
 
